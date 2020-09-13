b02f778ed06093791a82b3d2054edd3d11e40a74
DenisPerevoznik <den.perevoznik@gmail.com>
2020-09-13 14:42:21 +0300

    Finish project config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-import "./module"
-import "./scss/index.scss"
-console.log("Index.js working!");
+import './module';
+import './scss/index.scss';
 
-async function start(){
-    return await Promise.resolve('async working!');
-}
-
-start().then(console.log)
+console.log('Index.js working!');
